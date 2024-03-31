1fa45c6b4c94d110d8c7fd67a35c8a680444d72c
Pedro Veloso <contatopedrohalves@gmail.com>
2024-03-31 15:08:23 -0300

    first commit

---
+++ lib/megin.ex
@@ -0,0 +1,18 @@
+defmodule Megin do
+  @moduledoc """
+  Documentation for `Megin`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Megin.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/megin_test.exs
@@ -0,0 +1,8 @@
+defmodule MeginTest do
+  use ExUnit.Case
+  doctest Megin
+
+  test "greets the world" do
+    assert Megin.hello() == :world
+  end
+end
