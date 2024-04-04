2c4274fbf4962adb7d82f7c980428e0d13695a91
Pedro Veloso <contatopedrohalves@gmail.com>
2024-04-04 20:44:40 -0300

    feat: user creation route.

--- lib/megin_web/router.ex
+++ lib/megin_web/router.ex
@@ -1,6 +1,10 @@
 defmodule MeginWeb.Router do
   use MeginWeb, :router
 
+  post "/users" do
+    conn |> MeginWeb.UserController.create()
+  end
+
   post "/message" do
     conn |> MeginWeb.MessageController.create()
   end
