e186e2d45b6d13f2c3e6a55cf7bba2e3fb8a4bee
Pedro Veloso <contatopedrohalves@gmail.com>
2024-04-04 20:44:12 -0300

    feat: user controller, create user func.

---
+++ lib/megin_web/controllers/user_controller.ex
@@ -0,0 +1,15 @@
+defmodule MeginWeb.UserController do
+  alias Megin.Accounts
+
+  use MeginWeb, :controller
+
+  def create(conn) do
+    with {:ok, body} <- read_json_body(conn),
+         {:ok, struct} <- Accounts.create_user(body) do
+      send_json_resp(conn, 200, struct)
+    else
+      {:error, _reason} ->
+        send_json_resp(conn, 400, %{reason: "Invalid body."})
+    end
+  end
+end
